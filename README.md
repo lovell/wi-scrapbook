c574b2db5ef070e0d7dc95cb9e879a9feb3e4d23
Lovell Fuller <github@lovell.info>
2015-04-27 11:57:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+```sh
+git clone https://github.com/lovell/wi-scrapbook.git
+cd wi-scrapbook
+mkdir images tiles
+```
+
+Copy TIFF originals to `images` directory.
+
+```sh
+npm install
+npm run-script generate
+```
---
+++ generate.js
@@ -0,0 +1,50 @@
+// Convert TIF originals to Deep Zoom
+
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+const source = path.join(__dirname, 'images');
+const dest = path.join(__dirname, 'tiles');
+
+// Extracts page from input filename
+const pageFinder = /Page ([0-9]+[abc]?)/;
+
+// Pages-in-month
+const months = {};
+
+fs.readdirSync(source).forEach(function(month) {
+  const pages = [];
+  fs.readdirSync(path.join(source, month)).forEach(function(page) {
+    const pageNumber = pageFinder.exec(page);
+    if (pageNumber) {
+      pages.push(pageNumber[1]);
+      const inputFile = path.join(source, month, page);
+      const outputFilePrefix = path.join(dest, month + '-' + pageNumber[1]);
+      // Generate Deep Zoom
+      sharp(inputFile)
+        .toFile(outputFilePrefix + '.dzi')
+        .then(function() {
+          console.log('Generated DZI for page ' + pageNumber[1]);        
+        });
+      // Generate thumbnail
+      sharp(inputFile)
+        .resize(134, 178)
+        .sharpen()
+        .quality(75)
+        .trellisQuantisation()
+        .toFile(outputFilePrefix + '.jpg')
+        .then(function() {
+          console.log('Generated thumb for page ' + pageNumber[1]); 
+        });
+    } else {
+      console.log('Skipping ' + page);
+    }
+  });
+  months[month] = pages;
+});
+// Write pages-in-month data
+fs.writeFileSync(
+  path.join(__dirname, 'months.json'),
+  JSON.stringify(months, null, 2)
+);
